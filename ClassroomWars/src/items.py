32513683a6b2e14e6b38a4ce4f6ea2440187783d
Angelo Joachim <alpalma@up.edu.ph>
2024-12-09 22:53:20 +0800

    Update items.py

--- ClassroomWars/src/items.py
+++ ClassroomWars/src/items.py
@@ -1,1 +1,7 @@
-
+# Items that can be used during a turn
+items = {
+    "Chalk": {"type": "buff", "attack_buff": 10, "duration": 1, "description": "Increases attack by 10 for one turn."},
+    "Eraser": {"type": "buff", "defense_buff": 10, "duration": 1, "description": "Increases defense by 10 for one turn."},
+    "Scissors": {"type": "attack", "damage": 40, "description": "Deals a sharp 40 damage."},
+    "Book": {"type": "stamina", "stamina_gain": 20, "description": "Recovers 20 stamina points."},
+}
